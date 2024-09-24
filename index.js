ebd79f48c5ca4a66a6d2019293da59a7a50160b4
Salahuddin37 <mdsalahuddin.ms000@gmail.com>
2024-09-24 15:58:15 +0600

    history dynamic section done

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const noakhaliInputValue = getInputValueById('noakhali-input');
 const noakhaliTotalDonateValue = getTextValueById('noakhaliTotalValue');
 const floodAmount = getTextValueById('flood-total-amount');
 
+
+
 if(!isNaN(noakhaliInputValue) && noakhaliInputValue >0){
     if(floodAmount <= 0){
         alert('Invalid Input');
@@ -18,6 +20,21 @@ if(!isNaN(noakhaliInputValue) && noakhaliInputValue >0){
 
     const floodNewAmount = floodAmount - noakhaliInputValue;
     document.getElementById('flood-total-amount').innerText = floodNewAmount;
+// Modal Section------------------
+    const modal = document.getElementById('my_modal_5');
+    modal.showModal();
+
+// Noakhali History Section--------------
+    const historyItem = document.createElement("div");
+    historyItem.className = "bg-white w-full h-[100px] border border-gray-300 rounded-lg my-6";
+
+    historyItem.innerHTML =`
+    <p class="text-lg font-semibold py-3 px-3">${noakhaliInputValue} Taka is Donated for Donate for Flood at Noakhali, Bangladesh</p>
+    <p class="px-4 py-1">Date: ${new Date().toString()} </p>
+    
+    `
+    const historyContainer = document.getElementById('history-list');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 }
 else{
     alert('Invalid Input')
@@ -44,6 +61,21 @@ document.getElementById('feni-donate-btn')
 
         const floodNewAmount = floodAmount - feniInputValue;
         document.getElementById('flood-total-amount').innerText = floodNewAmount;
+// Modal Section------------------
+        const modal = document.getElementById('my_modal_5');
+        modal.showModal();
+
+// Feni History Section--------------
+        const historyItem = document.createElement("div");
+        historyItem.className =  "bg-white w-full h-[100px] border border-gray-300 rounded-lg my-6";
+
+        historyItem.innerHTML = `
+        <p class="text-lg font-semibold py-3 px-3">${feniInputValue} Taka is Donated for Donate for Flood Relief in Feni,Bangladesh </p>
+        <p class="px-4 py-1">Date: ${new Date().toString()} </p>
+        
+        `
+        const historyConatiner= document.getElementById('history-list');
+        historyConatiner.insertBefore(historyItem, historyConatiner.firstChild);
 
     }
     else{
@@ -54,7 +86,7 @@ document.getElementById('feni-donate-btn')
 // Quota Movement Section -----------------------------------Quota Movement
 document.getElementById('quota-btn')
 .addEventListener('click', function(){
-    console.log('Button Clicked')
+    
 
     const quotaInput = getInputValueById('quota-input');
     const quotaTotalDonate = getTextValueById('quota-total-donate');
@@ -70,9 +102,46 @@ document.getElementById('quota-btn')
 
         const newFloodAmount = floodAmount - quotaInput;
         document.getElementById('flood-total-amount').innerText = newFloodAmount;
+// Modal Section------------------
+        const modal = document.getElementById('my_modal_5');
+        modal.showModal();
+// Quota History Section--------------
+        const historyContainer = document.getElementById('history-list');
+        const historyItem = document.createElement('div');
+        historyItem.className = "bg-white w-full h-[100px] border border-gray-300 rounded-lg my-6";
+
+        historyItem.innerHTML = `
+        <p class="text-lg font-semibold py-3 px-3"> ${quotaInput} Taka is Donated for Aid for Injured in the Quota Movement </p>
+        <p class="px-4 py-1"> Date: ${new Date().toString()} </p>
+        `
+        const historyConatiner = document.getElementById('history-list');
+        historyConatiner.insertBefore(historyItem, historyConatiner.firstChild);
+        console.log('History item added:', historyItem);
     }
     else{
         alert('Invalid Input')
     }
 
+})
+
+
+// Button design section----------------------------------------btn design
+const historyBtn = document.getElementById('history-btn');
+const donationBtn = document.getElementById('donation-btn');
+
+historyBtn.addEventListener('click', function(){
+    historyBtn.classList.add("bg-lime-400", "hover:bg-lime-400");
+    donationBtn.classList.remove("bg-lime-400", "hover:bg-lime-400");
+
+    document.getElementById('sections').classList.add("hidden");
+    document.getElementById('history-list').classList.remove('hidden');  
+    console.log('History button clicked, sections hidden, history shown'); 
+});
+
+donationBtn.addEventListener('click', function(){
+    donationBtn.classList.add("bg-lime-400", "hover:bg-lime-400");
+    historyBtn.classList.remove("bg-lime-400", "hover:bg-lime-400");
+
+    document.getElementById('sections').classList.remove("hidden");
+    document.getElementById('history-list').classList.add("hidden");
 })
